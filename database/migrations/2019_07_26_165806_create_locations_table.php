e483df9f11bd2b089c488f56787176bcec65dea9
EB Ghartey <egbasie@st.ug.edu.gh>
2019-07-29 09:10:43 +0000

    locations table created

---
+++ database/migrations/2019_07_26_165806_create_locations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+             $table->string('name',64)->nullable();
+              $table->string('tag',64)->nullable();
+               $table->string('brand',64)->nullable();
+                $table->integer('organization_id')->unsigned()->index()->nullable();
+                $table->integer('parent_id')->unsigned()->index()->nullable();
+                $table->string('address',64)->nullable();
+                $table->integer('added_by_id')->unsigned()->index()->nullable();
+                $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
